c7103054bbb1a6080a2e267cf5a010ba07619b6b
Gustavo Luchetti <gustavo@luchetti.dev>
2025-07-10 20:34:37 -0300

    feat: re-ran drizzle migration

--- drizzle/0000_common_gertrude_yorkes.sql
+++ drizzle/0000_common_gertrude_yorkes.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `buyers` (
-	`id` integer PRIMARY KEY NOT NULL,
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`name` text NOT NULL,
 	`cpf` text NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE `items` (
-	`id` integer PRIMARY KEY NOT NULL,
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`order_id` integer NOT NULL,
 	`name` text NOT NULL,
 	`quantity` integer NOT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE `items` (
 );
 --> statement-breakpoint
 CREATE TABLE `orders` (
-	`id` integer PRIMARY KEY NOT NULL,
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`buyer_id` integer NOT NULL,
 	`status` text DEFAULT 'pendente' NOT NULL,
 	FOREIGN KEY (`buyer_id`) REFERENCES `buyers`(`id`) ON UPDATE no action ON DELETE no action
