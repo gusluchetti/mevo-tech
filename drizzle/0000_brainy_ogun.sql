cac778da205df0b39eb395c4e187470f00b62f34
Gustavo Luchetti <gustavo@luchetti.dev>
2025-07-10 20:05:50 -0300

    feat: init drizzle migration

---
+++ drizzle/0000_brainy_ogun.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `buyers` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`name` text NOT NULL,
+	`cpf` text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `items` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`order_id` integer NOT NULL,
+	`name` text NOT NULL,
+	`quantity` integer NOT NULL,
+	`price` integer NOT NULL,
+	FOREIGN KEY (`order_id`) REFERENCES `orders`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `orders` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`buyer_id` integer NOT NULL,
+	`status` text DEFAULT 'pendente' NOT NULL,
+	FOREIGN KEY (`buyer_id`) REFERENCES `buyers`(`id`) ON UPDATE no action ON DELETE no action
+);
