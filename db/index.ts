e6251ce7485f9a36102d532cc0ecb38b4ba340d9
Gustavo Luchetti <gustavo@luchetti.dev>
2025-07-10 19:12:42 -0300

    chore: separate creation from seeding

--- db/index.ts
+++ db/index.ts
@@ -4,7 +4,9 @@ import { CREATE_ORDERS } from "./order.models";
 import { CREATE_ITEMS } from "./item.models";
 
 export const db = new Database(":memory:");
+
 db.run(CREATE_BUYERS);
-db.run(`insert into buyers (name, cpf) values ('gustavo', '123');`)
 db.run(CREATE_ORDERS);
 db.run(CREATE_ITEMS);
+
+db.run(`insert into buyers (name, cpf) values ('gustavo', '123');`)
