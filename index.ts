bf727af4806a3d9afa02a91a4557016aeded70a9
Gustavo Luchetti <gustavo@luchetti.dev>
2025-07-10 19:07:07 -0300

    feat: add request validation w/ zod

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { OrderService } from "./modules/orders/services";
+import { validateRequest } from "zod-express-middleware";
+import { NewOrderParam, OrderIdParam, UpdateOrderParam } from "./modules/orders/dto";
 
 const app = express();
 app.use(express.json()) // for parsing application/json
@@ -9,9 +11,13 @@ const port = 8080;
 const orderService = new OrderService();
 
 app.get("/order", orderService.getOrders);
-app.get("/order/:id", orderService.getOrderById);
-app.post("/order", orderService.createOrder);
-app.put("/order/:id/:status", orderService.updateOrder);
+app.get("/order/:id", validateRequest({
+	params: OrderIdParam
+}), orderService.getOrderById);
+app.post("/order", validateRequest({ body: NewOrderParam }), orderService.createOrder);
+app.put("/order/:id/:status", validateRequest({
+	params: UpdateOrderParam
+}), orderService.updateOrder);
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}...`);
